5730f4f772e669caa291613908498fe844714351
Assaf Koren <assafnoahkoren@gmail.com>
2025-09-18 17:07:27 +0300

    .

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { codeInspectorPlugin } from 'code-inspector-plugin'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    codeInspectorPlugin({
+      bundler: 'vite',
+      hotKeys: ['altKey', 'shiftKey'], // Alt+Shift to toggle
+    }),
+    react(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
